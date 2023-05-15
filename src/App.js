45b77a0f82a2bd7decf5df097e8f215f6d277055
saitejakarnati <saitejakarnati555@gmail.com>
2023-05-15 12:18:49 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import NewStudent from './components/NewStudent';
 import Studentlist from './components/Studentlist';
 import StudentDetails from './components/Studentdetails';
@@ -8,7 +8,7 @@ import StudentDetails from './components/Studentdetails';
 function App() {
   const [students, setStudents] = useState([]);
   const [selectedId, setSelectedId] = useState(0)
-  const [currentPage, setCurrentPage] = useState("list")
+  //const [currentPage, setCurrentPage] = useState("list")
 
   function handleData(student) {
     localStorage.setItem(student.rollno, JSON.stringify(student));
@@ -27,23 +27,40 @@ function App() {
   function handleClick(id) {
     console.log(id);
     setSelectedId(id)
-    setCurrentPage("details")
+    //setCurrentPage("details")
   };
   function handleAdd() {
-    setCurrentPage("new")
+    //setCurrentPage("new")
   }
   function handleBack() {
-    setCurrentPage("list")
+   // setCurrentPage("list")
   }
 
   return (
     <div>
-      {currentPage === "new" && <NewStudent handleData={handleData} handleBack={handleBack} />}
-      {currentPage === "list" && <Studentlist students={students} handleClick={handleClick} handleAdd={handleAdd} />}
-      {currentPage === "details" && selectedId !== 0 && <StudentDetails id={selectedId} />}
+      <nav>
+        <div>
+          <Link to="/">Studentlist</Link>
+          <Link to="/NewStudent">NewStudent</Link>
+          <Link to="/Studentdetails">Studentdetails</Link>
+        </div>
+      </nav>
+      <div>
+        <Routes>
+          <Route path="/NewStudent" element={<NewStudent handleData={handleData} handleBack={handleBack} />} />
+          <Route path="/" element={<Studentlist students={students} handleClick={handleClick} handleAdd={handleAdd} />} />
+          <Route path="/Studentdetails" element={<StudentDetails id={selectedId} />} />
+        </Routes>
+      </div>
+      <div>
+        <div>{/*
+          {currentPage === "new" && <NewStudent handleData={handleData} handleBack={handleBack} />}
+          {currentPage === "list" && <Studentlist students={students} handleClick={handleClick} handleAdd={handleAdd} />}
+          {currentPage === "details" && selectedId !== 0 && <StudentDetails id={selectedId} />}
+  */}
+          </div>
+      </div>
     </div>
   );
 }
-
-
 export default App;
--- src/components/Studentlist.js
+++ src/components/Studentlist.js
@@ -1,3 +1,4 @@
+import React from "react";
 function Studentlist(props) {
     return (
       <div className="studentlist">
