e9d7c1c3315aa3a2af7176ca21e421b9cf91ae36
saitejakarnati <saitejakarnati555@gmail.com>
2023-05-25 18:51:19 +0530

    new

--- src/components/Studentdetails.js
+++ src/components/Studentdetails.js
@@ -3,13 +3,14 @@ import { useParams } from "react-router-dom";
 function StudentDetails() {
   const  {id1}  = useParams()
   console.log(id1)
-  console.log(JSON.parse(localStorage.getItem(id1)).name)
+  const student=JSON.parse(localStorage.getItem(id1))
+  console.log(student)
     return (
       <div className="studentdetails">
         <h2>Student Details</h2>
-        <p>Student Name: {JSON.parse(localStorage.getItem(id1)).name}</p>
-        <p>Roll Number: {JSON.parse(localStorage.getItem(id1)).rollno}</p>
-        <p>City: {JSON.parse(localStorage.getItem(id1)).city}</p>
+        <p>Student Name: {student.name}</p>
+        <p>Roll Number: {student.rollno}</p>
+        <p>City: {student.city}</p>
       </div>
     );
   }
